add55e2512294164016d1a7169d6c09ab7200466
Vadim Manushin <vadimmanushin@yandex.ru>
2023-03-08 19:38:19 +0300

    fix path in vivus

--- src/js/vivus.js
+++ src/js/vivus.js
@@ -1,3 +1,3 @@
 import Vivus from 'vivus';
 
-const icon = new Vivus('js-svg-icon-sign-up-form', { duration: 900, file: '../../assets/images/shape.svg' });
+const icon = new Vivus('js-svg-icon-sign-up-form', { duration: 900, file: './assets/images/shape.svg' });
